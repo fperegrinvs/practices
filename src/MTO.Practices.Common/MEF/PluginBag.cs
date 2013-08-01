4f6de5b0183e6f5cf9b052ab92f6083d4ea812b4
Juliano Bortolozzo Solanho <juliano.solanho@gmail.com>
2013-08-01 11:05:56 -0300

    Plugin Loader no practices

---
+++ src/MTO.Practices.Common/MEF/PluginBag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MTO.Practices.Common.MEF
+{
+    public abstract class PluginBag
+    {
+        protected void Load(string pattern = null)
+        {
+            PluginLoader.LoadPluginsFor(this, pattern);
+        }
+    }
+}
---
+++ src/MTO.Practices.Common/MEF/PluginLoader.cs
@@ -0,0 +1,79 @@
+﻿namespace MTO.Practices.Common.MEF
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using System.ComponentModel.Composition.Hosting;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+
+    /// <summary>
+    /// Loader de plugins do MEF
+    /// </summary>
+    public static class PluginLoader
+    {
+        /// <summary>
+        /// Carrega plugins para o aggregador de plugins definido
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="pattern"></param>
+        public static void LoadPluginsFor(PluginBag obj, string pattern = null)
+        {
+            //An aggregate catalog that combines multiple catalogs
+            var catalog = new AggregateCatalog();
+
+            var curPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var cats = SafeDirectoryCatalog(curPath, pattern);
+            foreach (var assemblyCatalog in cats)
+            {
+                catalog.Catalogs.Add(assemblyCatalog);
+            }
+
+            //Adds all the parts found in all assemblies in 
+            //the same directory as the executing program
+            string[] folders = System.IO.Directory.GetDirectories(curPath, "*", System.IO.SearchOption.AllDirectories);
+
+            foreach (string folder in folders)
+            {
+                cats = SafeDirectoryCatalog(folder, pattern);
+
+                foreach (var assemblyCatalog in cats)
+                {
+                    catalog.Catalogs.Add(assemblyCatalog);
+                }
+            }
+
+            //Create the CompositionContainer with the parts in the catalog
+            var container = new CompositionContainer(catalog);
+
+            //Fill the imports of this object
+            container.ComposeParts(obj);
+        }
+
+        private static IEnumerable<AssemblyCatalog> SafeDirectoryCatalog(string directory, string pattern = null)
+        {
+            var files = Directory.EnumerateFiles(directory, pattern ?? "*.dll", SearchOption.AllDirectories);
+
+            var result = new List<AssemblyCatalog>();
+
+            foreach (var file in files)
+            {
+                try
+                {
+                    var asmCat = new AssemblyCatalog(file);
+
+                    //Force MEF to load the plugin and figure out if there are any exports
+                    // good assemblies will not throw the RTLE exception and can be added to the catalog
+                    if (asmCat.Parts.ToList().Count > 0)
+                        result.Add(asmCat);
+                }
+                catch (ReflectionTypeLoadException ex)
+                {
+                    Logger.Instance.LogEvent(string.Join("\r\n", ex.LoaderExceptions.Select(x => x.ToString())));
+                }
+            }
+
+            return result;
+        }
+    }
+}
